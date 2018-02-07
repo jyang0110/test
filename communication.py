ced972d3780810daa2f75b377c35afd634fd4a7a
Pengyang Xiang <xiangpengyang.mun@gmail.com>
2018-02-07 16:20:46 -0600

    Update communication.py

--- communication.py
+++ communication.py
@@ -1,17 +1,19 @@
 import paramiko
 
+#Sending data from PC to raspberry Pi
 #t = paramiko.Transport('192.168.20.223','22')
 #t.connect(username = 'pi', password = '123')
 #sftp = paramiko.SFTPClient.from_transport(t)
-#remotepath='Windows.txt'
-#localpath='C:\Users\yangw\OneDrive\Desktop\Windows.txt'
+#remotepath='checkbutton.txt'
+#localpath='C:\Users\yangw\OneDrive\Desktop\checkbutton.txt'
 #sftp.put(localpath,remotepath)
 #t.close()
 
+#Downloading data from raspberry Pi to PC
 t = paramiko.Transport('192.168.20.223','22')
 t.connect(username = 'pi', password = '123')
 sftp = paramiko.SFTPClient.from_transport(t)
-remotepath= 'Windows.txt'
-localpath= 'C:\Users\yangw\OneDrive\Desktop\Windows.txt'
+remotepath= 'checkbutton.txt'
+localpath= 'C:\Users\yangw\OneDrive\Desktop\checkbutton.txt'
 sftp.get(remotepath, localpath)
 t.close()

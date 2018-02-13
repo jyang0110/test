85fa0644bc28dfe7af02b0f4d710b053d24baf4b
Pengyang Xiang <xiangpengyang.mun@gmail.com>
2018-02-12 23:49:43 -0600

    Update RaspberrypiSoftware.py

--- Software and Firmware/Source Code/ThirdBox_Files/RaspberrypiSoftware.py
+++ Software and Firmware/Source Code/ThirdBox_Files/RaspberrypiSoftware.py
@@ -5,7 +5,7 @@ import os.path
 import numpy as np
 import time
 
-#Set up the GPIO input and output
+#Setup the input and output of GPIO
 GPIO input and output
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
@@ -19,27 +19,30 @@ GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def read_temp():
-    #Check the first status of Thermometer sensor
+    #Check the first status of thermometer sensor
     Engine_check = os.path.isfile("/sys/bus/w1/devices/28-031724d850ff/w1_slave")
     if Engine_check is True:
         check_file = open("/sys/bus/w1/devices/28-031724d850ff/w1_slave")
         pre_check = check_file.read()
         firstline = pre_check.split("\n")[0]
         checkdata = firstline.split(" ")[11]
+    #Check the first/second status of thermometer sensor and the status of power switch
     if Engine_check is True and checkdata == 'YES' and (GPIO.input(24) == 0):
-    #Check the status of Thermometer sensor and the status of power switch
         tfile = open("/sys/bus/w1/devices/28-031724d850ff/w1_slave")
         text = tfile.read()
         tfile.close()
         secondline = text.split("\n")[1]
         temperaturedata = secondline.split(" ")[9]
         temp_d = float(temperaturedata[2:])
+        #Read the temperature as decimal number
         temp_d = temp_d / 1000
+        #Negative temperatures appear as a binary number in two’s complement on the seven bit display
         if temp_d < 0:
             temp_b = int(temp_d)
             temp_b = ~temp_b
             temp_b += 1
         else:
+        #Convert the temperature to binary number
             temp_b = int(temp_d)
             temp_b = '{0:07b}'.format(temp_b)
         led_a = str(temp_b)
@@ -50,10 +53,12 @@ def read_temp():
         led_5 = int(led_a[4])
         led_6 = int(led_a[5])
         led_7 = int(led_a[6])
+        #Get the status of push botton from checkbutton.txt
         open_button = open("/home/pi/checkbutton.txt")
         read_button = open_button.read()
         get_button = read_button.split(" ")[0]
-        if (GPIO.input(23) == 0) or get_button == 'True': #Check the status of push button
+        #If the button is pushed from real button or virtual button
+        if (GPIO.input(23) == 0) or get_button == 'True': 
             if led_1 == 1:
                 GPIO.output(21, True)
             if led_1 == 0:
@@ -82,6 +87,7 @@ def read_temp():
                 GPIO.output(5, True)
             if led_7 == 0:
                 GPIO.output(5, False)
+        #If the button is not  pushed, turn off the LEDs
         else:
             GPIO.output(21, False)
             GPIO.output(20, False)
@@ -91,14 +97,17 @@ def read_temp():
             GPIO.output(6, False)
             GPIO.output(5, False)
         res = str(temp_d)
+        #Save the temperature list to the savedData.txt
         with open("/home/pi/savedData.txt", "r+") as myfile:
             content = myfile.read()
             myfile.seek(0,0)
             myfile.write(res + '\n' + content)
             myfile.close()
+        #Save the last temperature data to the datafile.txt
         lastData = open('/home/pi/datafile.txt','w')
         lastData.write(res)
         lastData.close()
+        #Save the time stamp to the time.txt
         dataTime = open('/home/pi/time.txt','w')
         dataTime.write(str(time.time()))
         dataTime.close()
@@ -129,7 +138,7 @@ def read_temp():
         GPIO.output(13, False)
         GPIO.output(6, False)
         GPIO.output(5, False)
-
+        #Return -100 for unplugged temperature sensor
         with open("/home/pi/savedData.txt", "r+") as myfile:
             content = myfile.read()
             myfile.seek(0,0)
